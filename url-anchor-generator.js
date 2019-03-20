11584c4f4aeef38dbd5c8c41f173ce7ee1d4c04e
SerpSeed SEO <42698298+SerpSeed@users.noreply.github.com>
2019-03-20 10:33:02 +0000

    Removed jQuery

--- url-anchor-generator.js
+++ url-anchor-generator.js
@@ -1,168 +1,136 @@
-jQuery(document).ready(function() {
+var clearURLS = document.getElementById("clear-urls");
+var loadURLS = document.getElementById("load-urls");
+var urlsTextArea = document.getElementById("urls-textarea");
+var clearAnchors = document.getElementById("clear-anchors");
+var loadAnchors = document.getElementById("load-anchors");
+var anchorsTextArea = document.getElementById("anchors-textarea");
+var genericKeywords = document.getElementById("generic-keywords");
+var resultTextArea = document.getElementById("result-textarea");
+var joinURLS = document.getElementById("join-urls");
+var saveFile = document.getElementById("save-file");
+var loadFile = document.getElementById("load-file");
+var whichFile;
 
-  // Execute on button click
-  jQuery('button').click(function(e) {
 
-    // Check which button has been clicked - match each case
-    var idClicked = e.target.id;
+// Clear TextAreas
+clearURLS.addEventListener("click", function () {
+    urlsTextArea.value = "";
+});
 
-    switch (idClicked) {
+clearAnchors.addEventListener("click", function () {
+    anchorsTextArea.value = "";
+});
 
-      case "joinURLs": // Join URLs and Anchors w/ options
+// File Loading
+loadURLS.addEventListener("click", function () {
+    whichFile = "urls";
+    loadFile.click();
+});
 
-        // Read and clean URLs/Anchors from empty lines and whitespace, set variables
-        var urls = jQuery('#urls').val().split(/\r?\n/);
-        var anchors = jQuery('#anchors').val().split(/\r?\n/);
-        var urlsCleaned = urls.filter(n => n);
-        var anchorsCleaned = anchors.filter(n => n);
-        var urlsLength = urlsCleaned.length;
-        var anchorsLength = anchorsCleaned.length;
-        var result = [];
-        var rkey = 0;
+loadAnchors.addEventListener("click", function () {
+    whichFile = "anchors";
+    loadFile.click();
+});
 
-        // Set optional variables
-        var noFollow = jQuery('#noFollow').prop("checked");
-        var hrefTarget = jQuery('#hrefTarget').prop("checked");
-        var hrefTitle = jQuery('#hrefTitle').val();
-        var hrefLang = jQuery('#hrefLang').val();
-        var noFollowStr = "";
-        var hrefTargetStr = "";
-        var hrefTitleStr = "";
-        var hrefLangStr = "";
+loadFile.onchange = function () {
+    var file = loadFile.files[0];
 
-        // Check if any options have been enabled/set
-        if (noFollow) {
-          noFollowStr = " rel=\"nofollow\"";
-        }
-
-        if (hrefTarget) {
-          hrefTargetStr = " target=\"_blank\"";
-        }
-
-        if (hrefTitle.trim()) {
-          hrefTitleStr = " title=\"" + hrefTitle + "\"";
-        }
-
-        if (hrefLang.trim()) {
-          if (hrefLang.length === 2) {
-            hrefLangStr = " hreflang=\"" + hrefLang + "\"";
-          } else {
-            jQuery('#hrefLang').val("");
-            jQuery('#hrefLang').attr("placeholder", "Invalid Language Code")
-          }
-        }
-
-        // Generate URLs and Anchors
-        for (var u = 0; u < urlsLength; u++) {
-          for (var a = 0; a < anchorsLength; a++) {
-            result[rkey++] = "<a href=\"" + urls[u] + "\"" + noFollowStr + hrefTargetStr + hrefTitleStr + hrefLangStr + ">" + anchors[a] + "</a>"
-          }
-        }
-
-        // Print Result and break
-        jQuery('#result').val(result.join('\n'));
-        break;
-
-      case "clearURLs":	// Clear URL Textarea
-        e.preventDefault();
-        jQuery('#urls').val('');
-        break;
-
-      case "clearAnchors":	// Clear Anchor Textarea
-        e.preventDefault();
-        jQuery('#anchors').val('');
-        break;
-
-      case "loadURLs":	// Load URLs from file - Requires HTML input type="file"
-        jQuery('#loadURLFile').trigger('click');
-        break;
-
-      case "loadAnchors":	// Load Anchors from file - Requires HTML input type="file"
-        jQuery('#loadAnchorFile').trigger('click');
-        break;
-
-      case "genericKeywords":	// Load list of generic keywords from server source - Needs to be on same domain as script
-        jQuery.ajax({
-          url: "https://serpseed.com/wp-content/themes/serpseed/seo-tools/files/generic-keywords.txt",
-          dataType: "text",
-          success: function(data) {
-            jQuery('#anchors').val(data);
-          }
-        });
-        break;
-
-      case "saveFile":	// Save contents of results textarea to .TXT file
-        var textToSave = jQuery('#result').val();
-        var textToSaveAsBlob = new Blob([textToSave], {
-          type: "text/plain"
-        });
-        var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
-        var fileNameToSaveAs = jQuery('#saveFilename').val() + ".txt"
-
-        var downloadLink = document.createElement("a");
-        downloadLink.download = fileNameToSaveAs;
-        downloadLink.innerHTML = "Download File";
-        downloadLink.href = textToSaveAsURL;
-        downloadLink.onclick = destroyClickedElement;
-        downloadLink.style.display = "none";
-        document.body.appendChild(downloadLink);
-
-        downloadLink.click();
-        break;
-
-    } // End Switch Satement
-
-  });
-
-  // Handlers could be improved, need way to combine both into one.
-  // Handler for loading URL Files
-  jQuery('#loadURLFile').click(function() {
-    var fileInput = jQuery('#loadURLFile');
-    var fileDisplayArea = jQuery('#urls');
-
-    jQuery(fileInput).change(function() {
-      var file = jQuery(fileInput).prop('files')[0];
-      var textType = /text.*/;
-
-      if (file.type.match(textType)) {
+    if (file.type.match(/text.*/)) {
         var reader = new FileReader();
-        reader.onload = function() {
-
-          jQuery(fileDisplayArea).val(reader.result);
-
-        }
         reader.readAsText(file);
-      } else {
-        jQuery(fileDisplayArea).val("File not supported!");
-      }
-    });
-  });
-
-  // Handler for loading Anchor Files
-  jQuery('#loadAnchorFile').click(function() {
-    var fileInput = jQuery('#loadAnchorFile');
-    var fileDisplayArea = jQuery('#anchors');
-
-    jQuery(fileInput).change(function() {
-      var file = jQuery(fileInput).prop('files')[0];
-      var textType = /text.*/;
-
-      if (file.type.match(textType)) {
-        var reader = new FileReader();
-        reader.onload = function() {
-
-          jQuery(fileDisplayArea).val(reader.result);
-
+        
+        reader.onload = function () {
+            if (whichFile == "urls") {
+                urlsTextArea.value = reader.result;
+            } else {
+                anchorsTextArea.value = reader.result;
+            }
         }
-        reader.readAsText(file);
-      } else {
-        jQuery(fileDisplayArea).val("File not supported!");
-      }
-    });
-  });
+    } else {
+        readerResult = "File Not Support! Please use a TXT file.";
+    }
+};
 
-  function destroyClickedElement(event) {
-    document.body.removeChild(event.target);
-  }
+// Load List of Generic Keywords
+genericKeywords.addEventListener("click", function () {
+    var xhr = new XMLHttpRequest();
 
+    xhr.open("GET", "/tools/files/generic-keywords.txt");
+    xhr.onload = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            anchorsTextArea.value = xhr.responseText;
+        } else {
+            anchorsTextArea.value = "Loading Generic Keywords Failed!";
+        }
+    };
+    xhr.send();
+});
+
+// Generate URL + Anchor Combination
+joinURLS.addEventListener("click", function () {
+    var noFollow = document.getElementById("no-follow").checked;
+    var hrefTarget = document.getElementById("href-target").checked;
+    var hrefTitle = document.getElementById("href-title").value;
+    var hrefLang = document.getElementById("href-lang");
+    var noFollowStr = "";
+    var hrefTargetStr = "";
+    var hrefTitleStr = "";
+    var hrefLangStr = "";
+    var urls = urlsTextArea.value.split(/\r?\n/);
+    var anchors = anchorsTextArea.value.split(/\r?\n/);
+    var urlsCleaned = urls.filter(n => n);
+    var anchorsCleaned = anchors.filter(n => n);
+    var urlsLength = urlsCleaned.length;
+    var anchorsLength = anchorsCleaned.length;
+    var result = [];
+    var rkey = 0;
+
+    // Check if any options have been set
+    if (noFollow) {
+        noFollowStr = " rel=\"nofollow\"";
+    };
+
+    if (hrefTarget) {
+        hrefTargetStr = " target=\"_blank\"";
+    };
+
+    if (hrefTitle.trim()) {
+        hrefTitleStr = " title=\"" + hrefTitle + "\"";
+    };
+
+    if (hrefLang.value.trim()) {    // This is lazy validation
+        if (hrefLang.value.length <= 5) {
+            hrefLangStr = " hreflang=\"" + hrefLang.value + "\"";
+        } else {
+            hrefLang.setAttribute("placeholder", "Invalid Language Code");
+            hrefLang.value = "";
+        };
+    };
+
+    // Generate URLs and Anchors
+    var u;
+    var a;
+    for (u = 0; u < urlsLength; u++) {
+        for (a = 0; a < anchorsLength; a++) {
+            result[rkey++] = "<a href=\"" + urls[u] + "\"" + hrefTitleStr + hrefLangStr + noFollowStr + hrefTargetStr + ">" + anchors[a] + "</a>"
+        };
+    };
+
+    // Write Results
+    resultTextArea.value = result.join("\n");
+});
+
+// Save Results
+saveFile.addEventListener("click", function () {
+    var textToSaveAsBlob = new Blob([resultTextArea.value], { type: "text/plain" });
+    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+    var fileNameToSaveAs = document.getElementById("save-filename").value + ".txt";
+    var downloadLink = document.createElement("a");
+    
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.href = textToSaveAsURL;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
 });
